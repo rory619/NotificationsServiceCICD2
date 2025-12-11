694c5650f29d53da60d08659993dad71c09d438c
rory619 <118372080+rory619@users.noreply.github.com>
2025-12-11 19:46:34 +0000

    Fixing worker files

--- app/order_worker.py
+++ app/order_worker.py
@@ -1,25 +1,37 @@
+import aio_pika
+import asyncio
 import json
 import os
+
 RABBIT_URL = os.getenv("RABBIT_URL")
+
 EXCHANGE_NAME = "events_topic"
+
 
 async def main():
     conn = await aio_pika.connect_robust(RABBIT_URL)
     ch = await conn.channel()
 
+    # Declare the topic exchange
     ex = await ch.declare_exchange(EXCHANGE_NAME, aio_pika.ExchangeType.TOPIC)
 
+    # Queue for order events
     queue = await ch.declare_queue("order_events_queue")
 
+    
     await queue.bind(ex, routing_key="order.*")
+  
+
 
     print("Listening for order events (routing key: 'order.*')...")
 
     async with queue.iterator() as q:
         async for msg in q:
             async with msg.process():
+                
                 data = json.loads(msg.body)
                 print("Order Event:", msg.routing_key, data)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- app/worker.py
+++ app/worker.py
@@ -1,20 +1,28 @@
+import aio_pika
+import asyncio
 import json
 import os
+
 RABBIT_URL = os.getenv("RABBIT_URL")
 
+
 async def main():
+    # Connect to RabbitMQ
     connection = await aio_pika.connect_robust(RABBIT_URL)
     channel = await connection.channel()
+
 
     queue = await channel.declare_queue("orders_queue", durable=True)
 
     print("Waiting for messages on 'orders_queue'...")
 
+    
     async with queue.iterator() as q:
         async for message in q:
             async with message.process():
                 data = json.loads(message.body)
                 print("Received:", data)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
